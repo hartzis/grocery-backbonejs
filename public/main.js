07fa928e10ef2e2df6caac0b2187842f20778529
Brian Emil Hartz <brianhartz@gmail.com>
2014-08-12 21:48:11 -0600

    add cheese

--- public/main.js
+++ public/main.js
@@ -1,7 +1,14 @@
 (function() {
   var GroceryItem = Backbone.Model.extend({
-    Name: '',
-    Price: 0
+    defaults: {
+      Name: '',
+      Price: 0  
+    },
+    initialize: function() {
+      console.log('creating item');
+    }
+
+    
   })
 
   var GroceryList = Backbone.Collection.extend({
@@ -14,12 +21,19 @@
     },
     render: function() {
       // compile template using handlebars
-      var template = Handlebars.compile( $("#grocery_item_template").html() );
-
+      var template = Handlebars.compile( $("#grocery_item_template").html());
+      var html = template(this.model.toJSON());
       // Load the compiled HTML into the Backbone "el"
-      this.$el.html( template );
+      this.$el.html( html );
     }
-
   })
+
+  var milk = new GroceryItem({Name:'Milk'});
+  var cheese = new GroceryItem({Name:'Cheese', Price:1.2})
+
+  // create grocery list
+  var groceryList = new GroceryList;
+
+  var groceryItemView = new GroceryItemView({model: cheese, el:$('#main-container')});
 
 })();
